accb26a0410c06b9f322ceaab634a29d061c4de0
kumaraakarshan <aakarshan0005@gmail.com>
2023-10-01 18:48:17 +0530

    classs based added

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,7 @@
+import { Component } from "react";
+
 import classes from "./Counter.module.css";
-import { useSelector,useDispatch } from "react-redux";
+import { useSelector,useDispatch, connect } from "react-redux";
 
 const Counter = () => {
   const dispatch=useDispatch()
@@ -37,4 +39,61 @@ const decrementHandler5=()=>{
   );
 };
 
-export default Counter;
+export default Counter
+
+// class Counter extends Component{
+
+//   incrementHandler() {
+//     this.props.increment();
+
+//    }
+//   decrementHandler() { 
+//     this.props.decrement();
+//   }
+//   incrementHandler5() { 
+//     this.props.increment5();
+//   }
+//   decrementHandler5() {
+//     this.props.decrement5();
+//    }
+  
+//   toggleCounterHandler() { }
+  
+
+//   render() {
+//     return (
+//       <main className={classes.counter}>
+//         <h1>Redux Counter</h1>
+//         <div className={classes.value}>{this.props.counter}</div>
+//         <div>
+//           <button onClick={this.incrementHandler.bind}>Increment</button>
+//           <button onClick={this.decrementHandler.bind}>Decrement</button>
+//           <button onClick={this.incrementHandler5.bind}>Increment BY 5</button>
+//           <button onClick={this.decrementHandler5.bind}>Decrement BY 5</button>
+//         </div>
+//         <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+//       </main>
+//     );
+//   }
+// }
+
+// const mapStateToProps = state => {
+//   return {
+//     counter: state.counter
+    
+
+//   };
+// }
+
+// const mapDspatchToProps = dispatch => { 
+//   return {
+//     increment: () => dispatch({ type: 'increment' }),
+//     increment5: () => dispatch({ type: 'increment5' }),
+//     decrement: () => dispatch({ type: 'decrement' }),
+//     decrement5: () => dispatch({ type: 'decrement5' }),
+
+
+
+//   }
+// };
+// export default connect(mapStateToProps, mapDspatchToProps) (Counter);
